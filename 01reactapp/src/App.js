e679d632d93c024d1876b7373e08af633bb9296d
Jeevan Tambe <tambejeevan816@gmail.com>
2025-08-14 19:36:19 +0530

    Add first React App Using basic create-react-app.

---
+++ 01reactapp/src/App.js
@@ -0,0 +1,13 @@
+import One from "./One";
+
+function App() {
+  return (
+    <>
+    <h1>Jeevan Tambe</h1>
+    <One />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ 01reactapp/src/One.js
@@ -0,0 +1,16 @@
+function One() {
+  return (
+    <>
+      <h2>Hi there! I'm Jeevan Tambe, An Aspiring IT Professional Passionate About Technology And Software Development.</h2>
+      <p>
+        <h3>Install React App Process Dont Use It Use Vite</h3>
+        <h3># Clear old global installation </h3>
+        npm uninstall -g create-react-app 
+         <h3># Use latest via npx (no need to install globally)</h3>
+        npx create-react-app@latest 01reactapp
+      </p>
+    </>
+  );
+}
+
+export default One;
