1b475e4f4cf814d45585422cf436cd2ef97a77d9
Jeevan Tambe <tambejeevan816@gmail.com>
2025-08-16 19:45:45 +0530

    Add React.createElement method.

--- 01ViteApp/src/main.jsx
+++ 01ViteApp/src/main.jsx
@@ -1,9 +1,34 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+// const anotherElement = (
+//     <a href="https://google.com" target='_blank'>Visit google</a>
+// )
+
+
+
+const anotherUser = [1,2,3,4,5,'hello']
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank' },
+    'click me to visit google',
+    // anotherElement
+    anotherUser
+)
+
+
+createRoot(document.getElementById('root'))
+.render(
+    reactElement
 )
