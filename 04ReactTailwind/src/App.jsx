29adf9405a0f9b7981304612bf5d1a438013ce95
Jeevan Tambe <tambejeevan816@gmail.com>
2025-08-19 14:38:52 +0530

    Add Tailwind css configuration.

--- 04ReactTailwind/src/App.jsx
+++ 04ReactTailwind/src/App.jsx
@@ -1,19 +1,33 @@
-import Card from './Card'
-import './App.css'
+import { useState } from "react";
+import Card from "./Card";
+import "./App.css";
 
 function App() {
-  let myObj = {
-    username: "JEEVAN",
-    age: 22
-  }
-  let newArr = [1, 2, 3]
+  const [color, setColor] = useState("white");
+
+  // let myObj = {
+  //   username: "JEEVAN",
+  //   age: 22,
+  // };
+  // let newArr = [1, 2, 3];
+  
   return (
-    <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card username="Jeevan" btnText="click me"/>
-      <Card username="JEEVAN" />
-    </>
-  )
+    <div className="w-full h-screen duration-200"
+      style={{backgroundColor: color}}
+    >
+      <button className="mt-2 inline-flex cursor-pointer items-center text-lg font-semibold text-black px-5 py-5" 
+      onClick={() => setColor("oklch(70.9% 0.01 56.259)")}
+      > Change BackGround
+      </button>
+
+      <h1 className="text-black p-4 rounded-xl mb-4 ">
+        Tailwind test
+      </h1>
+      
+      <Card username="Jeevan" btnText="click me" />
+      <Card username="Jeevan" />
+    </ div>
+  );
 }
 
-export default App
+export default App;
